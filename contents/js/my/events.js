21f4547ecd77d45f2e042ac27659b35f4aa2ace6
shatterspike1 <stupidinternet303@gmail.com>
2015-02-20 17:12:39 -0700

    50/50 http://shatterspike1.github.io/

--- contents/js/my/events.js
+++ contents/js/my/events.js
@@ -2,6 +2,19 @@ function myEvents(){
     
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
+    $.get("https://api.github.com/users/shatterspike1/events", github, function(data) {
+        
+        var event = data
+        
+        console.log(event[0])
 
-    alert('todo')
+        $.get("/git-jquery/templates/eventList.jade", function(template) {
+
+            var html = jade.render(template, {items: event})
+            
+            $("#list").html(html)
+
+        })
+
+    })
 }
